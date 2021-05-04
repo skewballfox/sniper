42b05c569798483af0cb847f9251bd62f01ef71c
skewballfox <joshua.ferguson.273@gmail.com>
2021-05-04 18:53:35 -0500

    working on a faster snippet building strategy

--- sniper/src/snippetParser.rs
+++ sniper/src/snippetParser.rs
@@ -1,5 +1,10 @@
 use fancy_regex;
 
+enum SnipComponent {
+    tabstop{start_index: u32, end_index: u32},
+    metatabstop(u32,u32),
+    sub_snippet(u32,u32),
+}
 struct SnippetParser {
     base=fancy_regex::Regex;
 
