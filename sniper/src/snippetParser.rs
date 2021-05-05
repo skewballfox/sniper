8c2085dd409939a7e077766d5a75b5b6f5c81cf2
skewballfox <joshua.ferguson.273@gmail.com>
2021-05-05 13:31:03 -0500

    saving working state on main before branch

--- sniper/src/snippetParser.rs
+++ sniper/src/snippetParser.rs
@@ -1,17 +1,32 @@
-use fancy_regex;
 
-enum SnipComponent {
-    tabstop{start_index: u32, end_index: u32},
+pub(crate) enum SnipComponent {
+    tabstop {start: u32, end: u32},
     metatabstop(u32,u32),
-    sub_snippet(u32,u32),
+    sub_snippet{start: u32,end: u32, name: String},
 }
-struct SnippetParser {
-    base=fancy_regex::Regex;
+
+pub(crate) struct BuiltSnippetMetadata {
+    //subsnippet_tabstops: Hashmap<name:string,tabstops:vec<(u32,u32,u32)>?
+    tabstops: Vec<(u32,u32,u32)>
 
 }
 
-impl SnippetParser {
-    pub fn new()->{
-        base=fancy_regex::Regex::new("\${(\d|SNIPPET").unwrap()
+pub(crate) struct SnippetBuildMetadata {
+    name: String,
+    sub_snippet_count: u32,
+    tabstops: Vec<(u32,u32,u32)>,
+    body: Vec<Vec<SnipComponent>>,
+    sub_snippets:Vec<String>
+}
+
+impl SnippetBuildMetadata {
+    pub(crate) fn new(name:String,sub_snip_count:u32,sub_snips: Vec<String>)->Self {
+        Self {
+            name: name,
+            sub_snippet_count: sub_snip_count,
+            tabstops: Vec::new(),
+            body: Vec::new(),
+            sub_snippets: sub_snips,
+        }
     }
 }
