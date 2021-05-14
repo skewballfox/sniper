2274d707b69f895194a9f98b8f9a93f144abd800
skewballfox <joshua.ferguson.273@gmail.com>
2021-05-14 10:33:41 -0500

    progress on javascript client

--- sniper-common/src/client.rs
+++ sniper-common/src/client.rs
@@ -1,13 +1,11 @@
 
 use crate::service::SniperServiceClient;
-use tarpc::serde_transport;
+use tarpc::{serde_transport};
 use tokio::net::UnixStream;
 use tokio_serde::formats::Json;
 use tokio_util::codec::LengthDelimitedCodec;
 
-#[cfg(feature="client")]
 pub async fn init_client()-> SniperServiceClient {
-    let socket_path="/tmp/sniper.socket";
     let mut codec_builder=LengthDelimitedCodec::builder();
     let conn = UnixStream::connect(crate::SOCKET_PATH).await.unwrap();
     let framed_stream=codec_builder.new_framed(conn);
@@ -15,3 +13,7 @@ pub async fn init_client()-> SniperServiceClient {
     let transport = serde_transport::new(framed_stream, Json::default());
     SniperServiceClient::new(Default::default(),transport).spawn()
 }
+
+pub use tarpc::context::current as tarpc_context;
+pub use tokio;
+
