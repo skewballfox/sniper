efb7ad8ed1766a814841421a613bfd715189db45
skewballfox <joshua.ferguson.273@gmail.com>
2021-06-10 14:09:02 -0500

    commented out non-working code

--- sniper/src/main.rs
+++ sniper/src/main.rs
@@ -8,7 +8,7 @@ mod sniper_server;
 use config::SniperConfig;
 use daemonize::Daemonize;
 use dashmap::DashMap;
-use sniper_common::{service::SniperService, shutdown_tracer_provider};
+use sniper_common::service::SniperService; //, shutdown_tracer_provider};
 use snippet_manager::SnippetManager;
 use tokio_serde::formats::Bincode;
 
@@ -26,12 +26,13 @@ use crate::sniper_server::SniperServer;
 
 #[tokio::main]
 async fn main() {
+    //free the socket created by old instances
+    let _ = std::fs::remove_file(sniper_common::SOCKET_PATH);
+    /*
     //store stdout/stderr in files
     let stdout = std::fs::File::create("/tmp/sniper.out").unwrap();
     let stderr = std::fs::File::create("/tmp/sniper.err").unwrap();
 
-    //free the socket created by old instances
-    let _ = std::fs::remove_file(sniper_common::SOCKET_PATH);
     let daemon = Daemonize::new()
         .pid_file("/tmp/sniper.pid")
         //.chown_pid_file(true) // is optional, see `Daemonize` documentation
@@ -46,7 +47,7 @@ async fn main() {
         .exit_action(|| shutdown_tracer_provider())
         .start()
         .expect("failed to daemonize server");
-
+    */
     //initialize jaeger tracing
     sniper_common::init_tracing("Sniper Server").expect("failed to initialize tracing");
     //create a lister on the specified socket
@@ -68,28 +69,7 @@ async fn main() {
         let sniper_server =
             SniperServer::new(config.clone(), targets.clone(), snippet_manager.clone());
         let fut = server::BaseChannel::with_defaults(transport).execute(sniper_server.serve());
-        println!("request recieved");
+        println!("request received");
         tokio::spawn(fut);
     }
-}
-
-pub fn temp_dir() -> std::path::PathBuf {
-    let mut path = std::env::temp_dir();
-    path.push("sniper.pid");
-    let old_mask = unsafe { libc::umask(0) };
-    // Ignoring possible error during $TMPDIR/kak-lsp creation to have a chance to restore umask.
-    let _ = std::fs::DirBuilder::new()
-        .recursive(true)
-        .mode(0o1777)
-        .create(&path);
-    unsafe {
-        libc::umask(old_mask);
-    }
-    path.push(whoami::username());
-    std::fs::DirBuilder::new()
-        .recursive(true)
-        .mode(0o700)
-        .create(&path)
-        .unwrap();
-    path
 }
